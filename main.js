8f3dd988680cb2bca916ab4ce0402f2d02486410
DatBrian <vikard.2005@hotmail.com>
2023-03-08 09:08:36 -0500

    feat: :rocket: Operador &&
    En este bloque se muestra el operador && el cual nos permite comparar dos enunciados, si ambos son verdaderos nos retornará true mientras que en cualquier otro caso retornará false.

---
+++ main.js
@@ -0,0 +1,14 @@
+let operador = "logicos";
+console.log(operador);
+
+let case1 = true && true;
+console.log(case1);
+
+let case2 = true && false;
+console.log(case2);
+
+let case3 = false && false;
+console.log(case3);
+
+let case4 = false && true;
+console.log(case4);
