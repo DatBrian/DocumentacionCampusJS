e9d4f1608635326c92ad3ad8f28244f15f262f2e
DatBrian <vikard.2005@hotmail.com>
2023-03-09 03:32:34 -0500

    feat: :rocket: Operador de comparación mayor que (>)
    En este bloque se muestra el operador de comparación mayor que ">" el cual nos permite comparar si el operando izquierdo es mayor al operando derecho, como se puede ver en el primer ejemplo al imprimir en consola retorna false ya que no se cumple que el primer operando sea mayor al segundo mientras que en el otro ejemplo retorna true al imprimir en consola ya que el operando de la izquierda es mayor que el operando de la derecha

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
 let Op1 = 5;
 let Op2 = 10;
-console.log(Op1 !== Op2);
+console.log(Op1 > Op2);
 
-let Op3 = 15;
-let Op4 = "15";
-console.log(Op3 !== Op4);
-
-let Op5 = 20;
-let Op6 = 20;
-console.log(Op5 !== Op6);
+let Op3 = 20;
+let Op4 = 15;
+console.log(Op3 > Op4);
