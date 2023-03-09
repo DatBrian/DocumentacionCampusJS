538e7178dbe6b4372af624067cec3beba5935046
DatBrian <vikard.2005@hotmail.com>
2023-03-09 03:52:16 -0500

    feat: :rocket: Operador aritmético residuo (%)
    En este bloque se muestra el operador aritmético residuo "%" el cual permite obtener el residuo de la división entre dos operandos, como se ve en el ejemplo al realizar la operación nos va a retornar 0 que es el residuo entre dividir el primer operando con el segundo

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
 let Op1 = 10;
 let Op2 = 5;
-console.log(Op1 <= Op2);
-
-let Op3 = 15;
-let Op4 = 20;
-console.log(Op3 <= Op4);
-
-let Op5 = 25;
-let Op6 = 25;
-console.log(Op5 <= Op6);
+console.log(Op1 % Op2);
