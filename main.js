25c8e0961d495625fcaa5d471fbcccf12baf490a
DatBrian <vikard.2005@hotmail.com>
2023-03-09 03:43:08 -0500

    feat: :rocket: Operador de comparación menor o igual que (<=)
    En este bloque se muestra el operador menor o igual que "<=" el cual nos permite comparar si el operando de la izquierda es menor al operando de la derecha o si son iguales, como se ve en el primer ejemplo al tener en el operando izquierdo un valor mayor al operando derecho va a retornar un false a diferencia del segundo ejemplo en el que retorna true debido a que el operador izquierdo es menor al derecho y tambien del tercer ejemplo en el que retorna true porque aunque el operando izquierdo no es menor al operando izquierdos si son iguales

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-let Op1 = 5;
-let Op2 = 10;
-console.log(Op1 >= Op2);
+let Op1 = 10;
+let Op2 = 5;
+console.log(Op1 <= Op2);
 
-let Op3 = 20;
-let Op4 = 15;
-console.log(Op3 >= Op4);
+let Op3 = 15;
+let Op4 = 20;
+console.log(Op3 <= Op4);
 
 let Op5 = 25;
 let Op6 = 25;
-console.log(Op5 >= Op6);
+console.log(Op5 <= Op6);
